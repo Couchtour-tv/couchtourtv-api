a0bb87aa480ae7abcb34005e8e96ebd244020d6b
Michael Rinaldi <michael.rinaldi.01@gmail.com>
2021-02-22 16:53:44 -0500

    fixes env varbs mispell

--- lambdas/websockets/uploadMediaMeta.js
+++ lambdas/websockets/uploadMediaMeta.js
@@ -5,12 +5,12 @@ import { MediaMetaTableName } from '../common/constants';
 exports.handler = async event => {
     
     console.log('-----------------------------', '\n');
-    console.log(':: uploadMediatMeta.js', '\n');
+    console.log(':: [[ uploadMediatMeta.js ]]', '\n');
     const { connectionId, domainName, stage, requestId } = event.requestContext;
     const postData = JSON.parse(event.body).message;
 
     console.log('[12] postData:', postData);
-    Dynamo.write(postData, SocketTableName);
+    Dynamo.write(postData, MediaMetaTableName);
 
     console.log('[15] pre-return message');
     return Responses._200({ 
