0c66fc499892ac2b46e887bbf5b0dae128826529
Jon Gutwillig <programcodex@gmail.com>
2021-02-24 15:32:58 -0500

    commenting out redis

--- lambdas/websockets/getVideos.js
+++ lambdas/websockets/getVideos.js
@@ -1,26 +1,24 @@
 const AWS = require('aws-sdk');
 
-import { OptionsAPIGateway, OptionsDynamoDB, SocketTableName } from '../common/constants';
+import { OptionsAPIGateway } from '../common/constants';
 import Responses from '../common/API_Responses';
-// import Dynamo from '../common/Dynamo';\
 import dynamoDb from "../../libs/dynamodb-lib";
 
 import { MediaMetaTableName } from '../common/constants';
-import { v4 as uuidv4 } from 'uuid';
 
 
 exports.handler = async event => {
 
-    const { connectionId, domainName, stage, requestId } = event.requestContext;
+    const { connectionId } = event.requestContext;
     const socket = new AWS.ApiGatewayManagementApi(OptionsAPIGateway);
 
     const scan_params = { TableName: MediaMetaTableName };
     const video_array = await dynamoDb.scan(scan_params);
 
-    const replyMessage = { 
-        action: 'delivervideos', 
-        sender: connectionId, 
-        message: video_array.Items 
+    const replyMessage = {
+        action: 'delivervideos',
+        sender: connectionId,
+        message: video_array.Items
     };
 
     const socket_send = await socket.postToConnection({ ConnectionId: connectionId, Data: JSON.stringify(replyMessage) }).promise();
--- lambdas/websockets/uploadMediaMeta.js
+++ lambdas/websockets/uploadMediaMeta.js
@@ -6,26 +6,26 @@ import { v4 as uuidv4 } from 'uuid';
 
 exports.handler = async event => {
 
-    const { connectionId, domainName, stage, requestId } = event.requestContext;
+    // const { connectionId, domainName, stage, requestId } = event.requestContext;
     let postData = JSON.parse(event.body).message.values;
-    
+
     /*
         "ID" swap btwn payload and db write
         TODO -- make robust
 
-        uuid generationg for [artist and movie] is prototypical 
+        uuid generationg for [artist and movie] is prototypical
             just neeed a fast and dirty to prototype the db write
 
     */
-    postData.ID = postData.id
+    postData.ID = postData.id;
     postData.movieId = uuidv4();
     postData.artistId = uuidv4();
-    delete postData.id
+    delete postData.id;
 
     Dynamo.write(postData, MediaMetaTableName );
 
-    return Responses._200({ 
+    return Responses._200({
         success: true,
-        message: 'media_meta_uploaded' 
+        message: 'media_meta_uploaded'
     });
 };
