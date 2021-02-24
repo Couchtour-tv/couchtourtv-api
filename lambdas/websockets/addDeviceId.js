9d06d746cac6a78161a3739a7dae827b9403c4e8
Michael Rinaldi <michael.rinaldi.01@gmail.com>
2021-02-24 14:49:55 -0500

    adds devices table and socket endpoint for actiating device

--- lambdas/websockets/addDeviceId.js
+++ lambdas/websockets/addDeviceId.js
@@ -2,15 +2,15 @@ const AWS = require('aws-sdk');
 
 import { OptionsAPIGateway, OptionsDynamoDB, SocketTableName } from '../common/constants';
 import Responses from '../common/API_Responses';
-// import Dynamo from '../common/Dynamo';\
-import dynamoDb from "../../libs/dynamodb-lib";
+import Dynamo from '../common/Dynamo';
+// import dynamoDb from "../../libs/dynamodb-lib";
 
-import { MediaMetaTableName } from '../common/constants';
+import { DevicesTableName } from '../common/constants';
 import { v4 as uuidv4 } from 'uuid';
 
 /*
     SAMPLE PAYLOAD:
-        {"action": "getvideos"}
+        {"action": "add-device-id", "message": {"code": "123456"}}
 */
 
 exports.handler = async event => {
@@ -18,13 +18,17 @@ exports.handler = async event => {
     const { connectionId, domainName, stage, requestId } = event.requestContext;
     const socket = new AWS.ApiGatewayManagementApi(OptionsAPIGateway);
 
-    const scan_params = { TableName: MediaMetaTableName };
-    const video_array = await dynamoDb.scan(scan_params);
+    let postData = JSON.parse(event.body).message;
+    postData.ID = uuidv4();
+    postData.userId = uuidv4();
+    postData.deviceId = uuidv4();
+
+    Dynamo.write(postData, DevicesTableName );
 
     const replyMessage = { 
-        action: 'delivervideos', 
+        action: 'addDeviceId', 
         sender: connectionId, 
-        message: video_array.Items 
+        message: 'added device ids'
     };
 
     const socket_send = await socket.postToConnection({ 
@@ -33,12 +37,12 @@ exports.handler = async event => {
     }).promise();
 
     try {
-        console.log('\nGETVIDEOS-22 - Promise.all now ');
+        console.log('\nADDEVICEID-32 - Promise.all now ');
         await Promise.resolve( socket_send );
     } catch (e) {
-        console.log('\nGETVIDEOS-25 - error on promises', e.stack);
+        console.log('\nADDEVICEID-35 - error on promises', e.stack);
         return { statusCode: 500, body: e.stack };
     }
 
-    return Responses._200({ success: true, message: 'delivervideos' });
+    return Responses._200({ success: true, message: 'add-device-id' });
 };
