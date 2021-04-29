f41458d22c0325287aea883161f4d2b68a295f27
Jon Gutwillig <programcodex@gmail.com>
2021-04-29 09:26:07 -0400

    removing disconnect from sockets to test dynamodb in subnet

--- lambdas/websockets/disconnect.js
+++ lambdas/websockets/disconnect.js
@@ -1,13 +1,13 @@
 import { SocketTableName } from '../common/constants';
 import Responses from '../common/API_Responses';
-import Dynamo from '../common/Dynamo';
+// import Dynamo from '../common/Dynamo';
 
 exports.handler = async event => {
     console.log('WEBSOCKET DISCONNECT EVENT', event, '\n');
 
     const { connectionId } = event.requestContext;
 
-    await Dynamo.delete(connectionId, SocketTableName);
+    // await Dynamo.delete(connectionId, SocketTableName);
 
     return Responses._200({ message: 'disconnected' });
 };
