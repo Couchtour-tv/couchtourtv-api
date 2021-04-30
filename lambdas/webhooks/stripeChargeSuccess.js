dec45211d4c837e88d2407e5f590d42609e0319c
Jon Gutwillig <programcodex@gmail.com>
2021-04-30 09:43:32 -0400

    adding logs and data writes to checkout succes schema

--- lambdas/webhooks/stripeChargeSuccess.js
+++ lambdas/webhooks/stripeChargeSuccess.js
@@ -1,29 +1,50 @@
-// const AWS = require('aws-sdk');
+// const AWS = require('aws-sdk')
 
-import Responses from '../common/API_Responses';
-// import DynamoDb from '../../libs/dynamodb-lib';
+import Responses from '../common/API_Responses'
+import DynamoDb from '../../libs/dynamodb-lib'
 
 exports.handler = async event => {
     try {
-        const payload = JSON.parse(event.body);
+        const payload = JSON.parse(event.body)
         console.log("stripeChargeSuccess", payload)
 
-        // const stripeSuccessCheckoutWrite = {
-        //     TableName: StripeSuccessfulCheckoutTableName,
-        //     Item: {
-        //         ID: uuidv4(),
-        //         checkoutId: payload.id,
-        //         stripeCustomerId: payload.customer,
-        //         receivedAt: Date.now(),
-        //         payload: payload
-        //     }
-        // };
-        // await DynamoDb.put(stripeSuccessCheckoutWrite);
-        return Responses._200({ 'success': true });
+        if (payload.object === 'event') {
+
+            if (payload.data && payload.data.object.billing_details) {
+                console.log("BILLING DETAILS", payload.data.object.billing_details)
+            }
+
+            if (payload.data && payload.data.object.payment_method_details) {
+                console.log("PAYMENT METHOD DETAILS", payload.data.object.payment_method_details)
+            }
+
+            if (pauyload.data.object.paid === true) {
+
+                const stripeSuccessCheckoutWrite = {
+                    TableName: StripeSuccessfulCheckoutTableName,
+                    Item: {
+                        ID: uuidv4(),
+                        checkoutId: payload.id,
+                        stripeCustomerId: payload.data.object.customer,
+                        receivedAt: Date.now(),
+                        payload: payload
+                    }
+                }
+                await DynamoDb.put(stripeSuccessCheckoutWrite)
+
+            } else {
+                console.log("PAYMENT FAILED")
+            }
+
+        } else {
+            console.log("PAYLOAD NOT EVENT")
+        }
+
+        return Responses._200({ 'success': true })
 
     } catch (error) {
 
-        console.log('\n', error.stack);
-        return Responses._400({ 'success': false });
+        console.log('\n', error.stack)
+        return Responses._400({ 'success': false })
     }
-};
+}
