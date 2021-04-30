427f40ea523119121aa181db9dc5c92e0ffcd67b
Jon Gutwillig <programcodex@gmail.com>
2021-04-29 20:35:21 -0400

    removing the db write off stripe webhook until we know what we want

--- lambdas/webhooks/stripeSuccessfulCheckout.js
+++ lambdas/webhooks/stripeSuccessfulCheckout.js
@@ -1,25 +1,25 @@
 // const AWS = require('aws-sdk')
 
 import Responses from '../common/API_Responses'
-import { StripeSuccessfulCheckoutTableName } from '../common/constants'
-import DynamoDb from '../../libs/dynamodb-lib'
+// import { StripeSuccessfulCheckoutTableName } from '../common/constants'
+// import DynamoDb from '../../libs/dynamodb-lib'
 import { v4 as uuidv4 } from 'uuid'
 
 exports.handler = async event => {
     try {
         const payload = JSON.parse(event.body)
         console.log("stripeSuccessfulCheckout", payload)
-        const stripeSuccessCheckoutWrite = {
-            TableName: StripeSuccessfulCheckoutTableName,
-            Item: {
-                ID: uuidv4(),
-                checkoutId: payload.id,
-                stripeCustomerId: payload.customer,
-                receivedAt: Date.now(),
-                payload: payload
-            }
-        }
-        await DynamoDb.put(stripeSuccessCheckoutWrite)
+        // const stripeSuccessCheckoutWrite = {
+        //     TableName: StripeSuccessfulCheckoutTableName,
+        //     Item: {
+        //         ID: uuidv4(),
+        //         checkoutId: payload.id,
+        //         stripeCustomerId: payload.customer,
+        //         receivedAt: Date.now(),
+        //         payload: payload
+        //     }
+        // }
+        // await DynamoDb.put(stripeSuccessCheckoutWrite)
         return Responses._200({ 'success': true })
 
     } catch (error) {
