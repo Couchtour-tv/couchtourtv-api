0a98c7d9720b4b80437e2841b9e91d82308940a5
Michael Rinaldi <freshcatastrophe@richaelbot.fios-router.home>
2021-09-19 15:09:41 -0400

    debugging for api key error stripe

--- lambdas/common/API_Responses.js
+++ lambdas/common/API_Responses.js
@@ -23,6 +23,18 @@ const Responses = {
             body: JSON.stringify(data),
         };
     },
+    _500(data = {}) {
+        console.log("API_RESPONSES._500:", data);
+        return {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Origin': '*',
+            },
+            statusCode: 500,
+            body: JSON.stringify(data),
+        };
+    }
 };
 
 export default Responses;
--- lambdas/endpoints/stripe/createProduct.js
+++ lambdas/endpoints/stripe/createProduct.js
@@ -5,21 +5,28 @@ const stripe = require("stripe")(StripeSecretKey);
 exports.handler = async event => {
   console.log('createProduct-event ::', event);
   try {
-    const body = JSON.parse(event.body);
+  
+    // const body = JSON.parse(event.body);
+    // const { name, images, metadata, description } = body;
+    const body = await JSON.parse(event.body);
+    console.log('-Parsed Body: ', body);
+    console.log('-secret Key: ', StripeSecretKey);
+    console.log('-stripe Obj: ', stripe);
+
     const { name, images, metadata, description } = body;
     const product = await stripe.products.create({
-      name,
-      images,
-      metadata,
-      description
+      name, images, metadata, description
     });
     console.log("Create Product Price Object | Succeeded |:", product);
     return Responses._200(product);
+  
   } catch (error) {
+  
     console.log('Create Product Price Object | Error |', error);
     return Responses._400({
       message: "Create Product Price Object failed",
       success: false
     });
+  
   }
 };
