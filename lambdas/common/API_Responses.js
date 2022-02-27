0e527d8299dc0cf431eca065b5736df047689756
Daniel Stuart <dstuart85@yahoo.com>
2022-02-26 20:39:45 -0500

    attempting to redirect client to stripe portal session url: attempt 2

--- lambdas/common/API_Responses.js
+++ lambdas/common/API_Responses.js
@@ -1,41 +1,53 @@
 const Responses = {
-    _200(data = {}) {
-        console.log("API_RESPONSES._200:", data);
-        return {
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Methods': '*',
-                'Access-Control-Allow-Origin': '*',
-            },
-            statusCode: 200,
-            body: JSON.stringify({ ...data, ConnectionId: 'asdf'}),
-        };
-    },
-    _400(data = {}) {
-        console.log("API_RESPONSES._400:", data);
-        return {
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Methods': '*',
-                'Access-Control-Allow-Origin': '*',
-            },
-            statusCode: 400,
-            body: JSON.stringify(data),
-        };
-    },
-    _500(data = {}) {
-        console.log("API_RESPONSES._500:", data);
-        return {
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Methods': '*',
-                'Access-Control-Allow-Origin': '*',
-            },
-            statusCode: 500,
-            body: JSON.stringify(data),
-        };
+  _200(data = {}) {
+    console.log("API_RESPONSES._200:", data)
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 200,
+      body: JSON.stringify({ ...data, ConnectionId: "asdf" }),
     }
-};
+  },
+  _301(data = {}) {
+    console.log("API_RESPONSES._301:", data)
+    return {
+      headers: {
+        Location: data.url,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 301,
+      body: JSON.stringify({ ...data, ConnectionId: "asdf" }),
+    }
+  },
+  _400(data = {}) {
+    console.log("API_RESPONSES._400:", data)
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 400,
+      body: JSON.stringify(data),
+    }
+  },
+  _500(data = {}) {
+    console.log("API_RESPONSES._500:", data)
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 500,
+      body: JSON.stringify(data),
+    }
+  },
+}
 
-export default Responses;
-
+export default Responses
