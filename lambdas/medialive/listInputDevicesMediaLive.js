2208f88e94b8a62a924366c33dda2ddaa3515a2b
Jon Gutwillig <programcodex@gmail.com>
2022-07-14 14:16:27 -0400

    adding list all input device media live api route

---
+++ lambdas/medialive/listInputDevicesMediaLive.js
@@ -0,0 +1,28 @@
+// listInputDevicesMediaLive.js
+
+const AWS = require('aws-sdk');
+import Responses from '../common/API_Responses';
+import { OptionsMediaLive } from '../common/constants';
+
+
+exports.handler = async (event, context) => {
+    console.log("[10] listInputDevicesMediaLive", event, context);
+
+    var medialive = new AWS.MediaLive(OptionsMediaLive);
+    var params = {
+        MaxResults: 50
+    };
+
+    medialive.listInputDevices(params, function(err, data) {
+        if (err) {
+            console.log("[18] listInputDevicesMediaLive", err, err.stack);
+            return Responses._400({ 'success': false, 'data': err });
+        } else  {
+            console.log("[21] listInputDevicesMediaLive", data);
+            return Responses._200({ 'success': true, 'data': data.InputDevices });
+        }
+    });
+
+};
+
+// serverless invoke local --function list-input-devices-media-live
