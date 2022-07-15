f11b10ca2369a297d2d3155e2c8fb589b0573e6b
Jon Gutwillig <programcodex@gmail.com>
2022-07-14 20:44:26 -0400

    removing doc files and delateable files

--- tests/ingest_data/delete_vod_merchandise_by_id.py
+++
@@ -1,9 +0,0 @@
-import csv, requests, sys
-
-print('\n---------------')
-_id = str(sys.argv[1])
-api_resp = requests.post(
-	'http://localhost:3000/stg/api/v1/delete-vod-merchandise-by-id',
-	data = { 'vod_id': _id }
-)
-print(api_resp.status_code)
