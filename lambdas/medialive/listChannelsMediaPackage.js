3c0768e383c4a03018f37237d4879a673d103175
dan <stuheadz@gmail.com>
2022-11-19 15:58:01 -0500

    puts response in json format

--- lambdas/medialive/listChannelsMediaPackage.js
+++ lambdas/medialive/listChannelsMediaPackage.js
@@ -1,54 +1,52 @@
 // listChannelsMediaPackage.js
 
-const AWS = require('aws-sdk');
-import Responses from '../common/API_Responses';
-import { OptionsMediaPackage } from '../common/constants';
+const AWS = require("aws-sdk")
+import Responses from "../common/API_Responses"
+import { OptionsMediaPackage } from "../common/constants"
 
 function sanitizeObject(obj) {
-    return { Id: obj.Id, Description: obj.Description };
+  return { Id: obj.Id, Description: obj.Description }
 }
 
 exports.handler = async (event, context) => {
-    console.log("[10] listChannelsMediaPackage", event, context);
-    var mediapackage = new AWS.MediaPackage(OptionsMediaPackage);
-    var params = {
-        MaxResults: 50
-    };
+  console.log("[10] listChannelsMediaPackage", event, context)
+  var mediapackage = new AWS.MediaPackage(OptionsMediaPackage)
+  var params = {
+    MaxResults: 50,
+  }
 
-    let successOpt = false;
-    let dataOpt;
+  let successOpt = false
+  let dataOpt
 
-    try {
-
-        const resp = await mediapackage.listChannels(params, function(err, data) {
-            if (err) {
-                console.log("[18] listChannelsMediaPackage", err, err.stack);
-                return err;
-            } else  {
-                console.log("[21] listChannelsMediaPackage", data);
-                successOpt = true;
-                return data;
-            }
-        }).promise();
-
-        if (successOpt) {
-            dataOpt = resp.Channels.map(sanitizeObject);
+  try {
+    const resp = await mediapackage
+      .listChannels(params, function (err, data) {
+        if (err) {
+          console.log("[18] listChannelsMediaPackage", err, err.stack)
+          return err
         } else {
-            dataOpt = resp;
-        };
+          console.log("[21] listChannelsMediaPackage", data)
+          successOpt = true
+          return data
+        }
+      })
+      .promise()
 
-        console.log("[32] listChannelsMediaPackage", resp);
-        return Responses._200({ 'success': true, 'data': dataOpt });
-
-    } catch (error) {
-
-        console.log("[37] listChannelsMediaPackage", error)
-        return Responses._404({
-          message: "listChannelsMediaPackage failed",
-          success: successOpt,
-        })
+    if (successOpt) {
+      dataOpt = resp.Channels.map(sanitizeObject)
+    } else {
+      dataOpt = resp
     }
 
-};
+    console.log("[32] listChannelsMediaPackage", resp)
+    return Responses._200({ success: true, data: dataOpt })
+  } catch (error) {
+    console.log("[37] listChannelsMediaPackage", error)
+    return Responses._404({
+      message: "listChannelsMediaPackage failed",
+      success: successOpt,
+    })
+  }
+}
 
 // sls invoke local --function list-channels-media-package
