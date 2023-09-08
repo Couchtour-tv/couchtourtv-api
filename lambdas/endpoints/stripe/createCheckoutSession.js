04623ca6577aab9c3acb6ecb69f2c76412eb4ad6
dan <stuheadz@gmail.com>
2023-09-07 22:18:52 -0400

    adds stripe checkout session

---
+++ lambdas/endpoints/stripe/createCheckoutSession.js
@@ -0,0 +1,63 @@
+import Responses from "../../common/API_Responses"
+import { StripeSecretKey } from "../../common/constants"
+const stripe = require("stripe")(StripeSecretKey)
+
+exports.handler = async (event) => {
+  console.log("Create Checkout Session ::", event)
+  try {
+    // const body = await JSON.parse(event.body)
+    // const { priceId } = body
+
+    const session = await stripe.checkout.sessions.create({
+      expand: ["line_items"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "VIP",
+            },
+            unit_amount: 0,
+            tax_behavior: "exclusive",
+          },
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+            maximum: 8,
+          },
+        },
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "General Admission",
+            },
+            unit_amount: 0,
+            tax_behavior: "exclusive",
+          },
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+            maximum: 8,
+          },
+        },
+      ],
+      automatic_tax: {
+        enabled: true,
+      },
+      mode: "payment",
+      success_url: "https://beta.couchtour.tv?success=true",
+      cancel_url: "https://beta.couchtour.tv?canceled=true",
+    })
+
+    console.log("Create Checkout Session | Succeeded |:", session)
+    res.redirect(303, session.url)
+    return Responses._200(session)
+  } catch (error) {
+    console.log("Create Checkout Session | Error |", error)
+    return Responses._500({
+      message: "Create Checkout Session failed",
+      success: false,
+    })
+  }
+}
